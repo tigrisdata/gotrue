8964dcec825cee5464fd795bd4b0f1beb6bde46c
Jigar Joshi <jigarjm@gmail.com>
2023-03-03 15:24:11 -0800

    fix: Ignore the superadmin user creation error (#21)

--- hack/create_user.sh
+++ hack/create_user.sh
@@ -2,7 +2,7 @@
 
 CREATE=${CREATE_SUPER_ADMIN_USER:false}
 if [ "${CREATE}" == "true" ]; then
-  gotrue admin --instance_id="$GOTRUE_INSTANCE_ID" --aud="$GOTRUE_SUPERADMIN_AUD" --superadmin=true createuser "$GOTRUE_SUPERADMIN_USERNAME" "$GOTRUE_SUPERADMIN_PASSWORD"
+  gotrue admin --instance_id="$GOTRUE_INSTANCE_ID" --aud="$GOTRUE_SUPERADMIN_AUD" --superadmin=true createuser "$GOTRUE_SUPERADMIN_USERNAME" "$GOTRUE_SUPERADMIN_PASSWORD" || true
 else
   echo "Skipped creation of user"
 fi
