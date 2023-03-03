f1434db4d777a7e6bab5984cfae8f5e14ede5068
Jigar Joshi <jigarjm@gmail.com>
2023-03-03 10:22:27 -0800

    fix: Fixed the create user script for ash

--- hack/create_user.sh
+++ hack/create_user.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CREATE=${CREATE_SUPER_ADMIN_USER:false}
-if [[ $CREATE == "true" ]]; then
+if [ "${CREATE}" == "true" ]; then
   gotrue admin --instance_id="$GOTRUE_INSTANCE_ID" --aud="$GOTRUE_SUPERADMIN_AUD" --superadmin=true createuser "$GOTRUE_SUPERADMIN_USERNAME" "$GOTRUE_SUPERADMIN_PASSWORD"
 else
   echo "Skipped creation of user"
