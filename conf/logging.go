3efe08e52304dafdb7ad84362179af57acfb6a9c
Jigar Joshi <jigarjm@gmail.com>
2023-03-22 17:20:31 -0700

    ci: Debug ci failures

--- conf/logging.go
+++ conf/logging.go
@@ -12,13 +12,13 @@ import (
 )
 
 type LoggingConfig struct {
-	Level            string                 `mapstructure:"log_level" json:"log_level"  default:"console"` // defaults to std log - developer friendly
+	Level            string                 `mapstructure:"log_level" json:"log_level"  default:"debug"` // defaults to std log - developer friendly
 	File             string                 `mapstructure:"log_file" json:"log_file"`
 	DisableColors    bool                   `mapstructure:"disable_colors" split_words:"true" json:"disable_colors"`
 	QuoteEmptyFields bool                   `mapstructure:"quote_empty_fields" split_words:"true" json:"quote_empty_fields"`
 	TSFormat         string                 `mapstructure:"ts_format" json:"ts_format"`
 	Fields           map[string]interface{} `mapstructure:"fields" json:"fields"`
-	Format           string                 `mapstructure:"format" json:"format" default:"stdout"`
+	Format           string                 `mapstructure:"format" json:"format" default:"console"`
 }
 
 // trim full path. output in the form directory/file.go.
